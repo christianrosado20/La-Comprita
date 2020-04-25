2cbf6ee98970e07825bc52495dfb668e38a59389
Orlando Marrero <orlando.marrero6@upr.edu>
2020-04-25 19:52:32 -0400

    Connecting Database with the app

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // Mongo URI: add user credentials
 
 module.exports = {
-    mongoURI: 'mongodb+srv://<username>:<password>*@la-compritadb-m5jxg.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://orlando_marrero:orlando123*@la-compritadb-m5jxg.mongodb.net/test?retryWrites=true&w=majority',
     secretOrKey: 'secret'
 }
