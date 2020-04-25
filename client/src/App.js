49c086c6097fbc88ea7ffb5b4e399ec61c8a9c57
Yaritza Garcia <yaritza.garcia@nagnoi.com>
2020-04-25 14:46:10 -0400

    ResultPage in process

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import {
 // Components
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
+import ResultPage from './components/ResultPage';
 
 
 class App extends Component {
@@ -18,6 +19,7 @@ class App extends Component {
           <Header></Header>
           <Switch>
             <Route exact path="/" component={LandingPage} />
+            <Route exact path="/Resultado" component={ResultPage} />
           </Switch>
         </div>
       </Router>
