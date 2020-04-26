5ab9efbb7c452fd9bb4607920bbe79926f2373cd
Christian Rosado <christian.rosado20@upr.edu>
2020-04-26 15:36:56 -0400

    push to heroku

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -16,6 +16,7 @@ class Product extends Component {
             {/* Information */}
             <div className="Product__information"> 
                  <h3>{data.name}</h3>
+                 <p>{data.store}</p>
                   <h5>$ {(data.price / 100).toFixed(2)}</h5>
                  <p>{data.description}</p>
                  <p>Inventory: {(data.inventory) >= 100 ? 'HIGH' : 'LOW'}</p>
